a24e02bc4cdc165def8027dd92dc2736e2a73b72
kevinolweb <kevin.oleary91@gmail.com>
2023-04-15 22:23:29 +0000

    test 4

--- cuforum/urls.py
+++ cuforum/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',include('socialapp.urls')),
+    path('/home',include('socialapp.urls')),
 ]
