cea3fff43fbb8f7285e4a3a8540d23ed6ef2bf1e
kevinolweb <kevin.oleary91@gmail.com>
2023-04-16 13:38:01 +0000

    Fix applied

--- cuforum/settings.py
+++ cuforum/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dj_database_url
